1bc46fc07e85f78f6a7b385088cfe90536c3069a
HET2912 <hetpatel29122004@gmail.com>
2025-07-17 10:21:10 +0530

    Test to check adding a valid sweet to sweets shop management system

---
+++ src/SweetShopManagementSystem.js
@@ -0,0 +1,20 @@
+const Sweet = require('./sweet.js');
+
+class SweetShopManagementSystem {
+    #availableSweets = [];
+    #soldSweets = [];
+
+    // Get a list of available sweets
+    getAvailableSweets() {
+        return [...this.#availableSweets];
+    }
+     // Get a list of sold sweets
+    getSoldSweets() {
+        return [...this.#soldSweets];
+    }
+      // Add a sweet to the shop
+    addSweet(sweet) {
+        this.#availableSweets.push(sweet);
+        console.log(`Sweet "${sweet.name}" added successfully!`);
+    }
+}
